7652270e84fe2470d683fd7b6860eed0ecd1b03b
Teresa Alonso <tere.alga22@gmail.com>
2025-04-08 22:14:01 -0600

    (;

---
+++ src/main/java/com/ebc/fizzbuzz/controller/Homecontroller.java
@@ -0,0 +1,16 @@
+package com.ebc.fizzbuzz.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/") // Define la raíz como ruta base
+
+public class Homecontroller {
+    @GetMapping
+    public String home() {
+        return "¡Hola mundo! CI/CD Listo!";
+    }
+
+}
